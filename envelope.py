cc2054aab7504941cb95813d7aa866e912c39470
三ッ國拓真 <lupin.create@hotmail.co.jp>
2014-05-26 18:02:43 +0900

    ゼミ課題　製作中

---
+++ envelope.py
@@ -0,0 +1,26 @@
+def f(x,t):
+   return t * x - t**2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def subplots():
+    "Custom subplots with axes throught the origin"
+    fig, ax = plt.subplots()
+
+    # Set the axes through the origin
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_position('zero')
+    for spine in ['right', 'top']:
+        ax.spines[spine].set_color('none')
+    
+    return fig, ax
+
+
+fig, ax = subplots()  # Call the local version, not plt.subplots()
+x = np.linspace(-2, 10, 200)
+y = f(x,t=1)
+ax.plot(x, y, 'r-', linewidth=2, label='sine function', alpha=0.6)
+ax.legend(loc='lower right')
+plt.show()
+
+
