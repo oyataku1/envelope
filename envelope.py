0416d9f0d9987fe90af277663e64ea30a953ad31
三ッ國拓真 <lupin.create@hotmail.co.jp>
2014-05-31 22:00:54 +0900

    後少しで完成

--- envelope.py
+++ envelope.py
@@ -1,7 +1,10 @@
-def f(x,t):
-   return t * x - t**2
+
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as np  #Pythonにおいて数値計算を効率的に行うための拡張モジュール
+def f(x,t):     
+   return t * x - t**2 #包絡線のもとの関数を定義
+
+u = 0.1  #後に非正数値刻みにするために予め定義しておく
 
 def subplots():
     "Custom subplots with axes throught the origin"
@@ -17,10 +20,20 @@ def subplots():
 
 
 fig, ax = subplots()  # Call the local version, not plt.subplots()
-x = np.linspace(-2, 10, 200)
-y = f(x,t=1)
-ax.plot(x, y, 'r-', linewidth=2, label='sine function', alpha=0.6)
-ax.legend(loc='lower right')
+x = np.linspace(-1, 1, 200)　#xの範囲設定
+plt.xlim([x.min(),x.max()]) #x軸の範囲
+plt.ylim([-100,200])        #y軸の範囲
+ax.set_xticks([]) #x,y軸の目盛消し
+ax.set_yticks([]) 
+for  n in range(-10, 11):　　#-10~10の範囲でまわす
+    y = f(x,t = n * u)      #u=0.1よりtは0.1刻みになる
+    ax.plot(x, y, 'r-', linewidth=2, label='sine function', alpha=0.6)
+
 plt.show()
+
+
+
+
+
 
 
