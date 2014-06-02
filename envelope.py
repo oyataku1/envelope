1d9231a89398b8c90eb5a0b5a2dccb71752e25ce
三ッ國拓真 <lupin.create@hotmail.co.jp>
2014-06-02 15:03:37 +0900

    包絡線ver.2

--- envelope.py
+++ envelope.py
@@ -1,9 +1,9 @@
-import numpy as np
+import matplotlib.pyplot as plt
 import pylab as pl      #Pythonにおいて数値計算を効率的に行うための拡張モジュール
 def f(x,t):             #包絡線のもとの関数を定義
    return t * x - t**2
 
-u = 0.5  　　　#後に非正数値刻みにするために予め定義しておく
+u = 0.5     #後に非正数値刻みにするために予め定義しておく
 
 def subplots():
     "Custom subplots with axes throught the origin"
@@ -19,14 +19,14 @@ def subplots():
 
 
 fig, ax = subplots()  # Call the local version, not plt.subplots()
-x = np.linspace(-15, 15, 100)　#xの範囲設定
-plt.xlim([x.min(),x.max()])  #y軸の範囲
-plt.ylim([-30,40])　　　　
-ax.set_xticks([])　　　　#x,y軸の目盛消し
-ax.set_yticks([]) 
+x = np.linspace(-15, 15, 100) #xの範囲設定
+plt.xlim([x.min(),x.max()])   #y軸の範囲
+plt.ylim([-30,40])
+ax.set_xticks([])   #x,y軸の目盛消し
+ax.set_yticks([])
 fig.suptitle('Envelope Theorem', fontsize=15)
-for  n in range(-10, 11):　　　　　#-10~10の範囲でまわす
-    y = f(x,t = n * u)　　　　　　　#u=0.5よりtは0.5刻みになる
+for  n in range(-10, 11):  #-10~10の範囲でまわす
+    y = f(x,t = n * u)     #u=0.5よりtは0.5刻みになる
     ax.plot(x, y, 'r-', linewidth=1.0, label='sine function', alpha=0.6)
     
 
